2a3c613622e64299ef411974d75aa7ec6b8c0dd8
nulzi <kimty131@naver.com>
2022-11-29 23:39:02 +0900

    [Add] MovePostPage
    - view: MovePostPage.vue
    - component: MenuBar, progressMenu

--- postrofit/src/App.vue
+++ postrofit/src/App.vue
@@ -1,19 +1,25 @@
 <template>
-  <line-2></line-2>
+  <!-- <line-2></line-2> -->
   <Test></Test>
 </template>
 
 <script>
-import Test from "./views/MainPage.vue";
-import Line2 from "./components/SubWay/Line2.vue";
+// import Test from "./views/MainPage.vue";
+import Test from "./views/MovePostPage.vue";
+// import Line2 from "./components/SubWay/Line2.vue";
 
 export default {
   name: "App",
   components: {
     Test,
-    Line2,
+    // Line2,
   },
 };
 </script>
 
-<style></style>
+<style>
+body {
+  margin: 0;
+  padding: 0;
+}
+</style>
