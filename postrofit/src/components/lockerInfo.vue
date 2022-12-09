0d4e80c06c45811583dd98e6c63736b78215e394
nulzi <kimty131@naver.com>
2022-12-09 14:27:12 +0900

    [Add] components
    - bottomLocker
    - entrustMenu
    - lockerInfo
    - lockerModal

---
+++ postrofit/src/components/lockerInfo.vue
@@ -0,0 +1,113 @@
+<template>
+  <div class="lockerInfo_container">
+    <div class="lockerInfo_locker"></div>
+    <div class="lockerInfo_state">
+      <div class="lockerInfo_state_column">
+        <div class="lockerInfo_state_row">
+          <div class="lockerInfo_state_color lockerInfo_state_using"></div>
+          <div class="lockerInfo_state_desc">사용중</div>
+        </div>
+        <div class="lockerInfo_state_row">
+          <div class="lockerInfo_state_color lockerInfo_state_repair"></div>
+          <div class="lockerInfo_state_desc">점검중</div>
+        </div>
+      </div>
+      <div class="lockerInfo_state_column">
+        <div class="lockerInfo_state_row">
+          <div class="lockerInfo_state_color lockerInfo_state_empty"></div>
+          <div class="lockerInfo_state_desc">선택가능</div>
+        </div>
+        <div class="lockerInfo_state_row">
+          <div class="lockerInfo_state_color lockerInfo_state_controller"></div>
+          <div class="lockerInfo_state_desc">제어부</div>
+        </div>
+      </div>
+      <div class="lockerInfo_state_column">
+        <div class="lockerInfo_state_row">
+          <div class="lockerInfo_state_color lockerInfo_state_chosen"></div>
+          <div class="lockerInfo_state_desc">선택</div>
+        </div>
+        <div class="lockerInfo_state_row">
+          <div class="lockerInfo_state_color lockerInfo_state_mine"></div>
+          <div class="lockerInfo_state_desc">내 보관함</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.lockerInfo_container {
+  padding: 18px 0px 24px 43px;
+}
+.lockerInfo_locker {
+  margin-bottom: 42px;
+  height: 342px;
+  width: 500px;
+  background: gray;
+}
+.lockerInfo_state {
+  margin-left: 6px;
+  display: flex;
+}
+.lockerInfo_state_column {
+  display: flex;
+  flex-direction: column;
+}
+.lockerInfo_state_row {
+  display: flex;
+  margin: 0px 0px 10px;
+}
+.lockerInfo_state_color {
+  margin: 0px 5px 0px 0px;
+  /* Layout Properties */
+  /* top: 549px; */
+  /* left: 49px; */
+  width: 29px;
+  height: 18px;
+  /* UI Properties */
+  /* background: 0% 0% no-repeat padding-box; */
+  border-radius: 5px;
+  opacity: 1;
+}
+.lockerInfo_state_using {
+  background: #e4e4e4 0% 0% no-repeat padding-box;
+}
+.lockerInfo_state_empty {
+  background: #5e62d1 0% 0% no-repeat padding-box;
+}
+.lockerInfo_state_chosen {
+  background: #d04040 0% 0% no-repeat padding-box;
+}
+.lockerInfo_state_repair {
+  background: #707070 0% 0% no-repeat padding-box;
+}
+.lockerInfo_state_controller {
+  background: #6fbb69 0% 0% no-repeat padding-box;
+}
+.lockerInfo_state_mine {
+  background: #ffc702 0% 0% no-repeat padding-box;
+}
+.lockerInfo_state_desc {
+  margin: 0px 5px 0px 0px;
+  /* Layout Properties */
+  /* top: 550px; */
+  /* left: 83px; */
+  /* width: 36px; */
+  /* height: 16px; */
+  /* UI Properties */
+  font: var(--unnamed-font-style-normal) normal var(--unnamed-font-weight-bold)
+    12px/16px Roboto;
+  letter-spacing: var(--unnamed-character-spacing-0);
+  color: var(--unnamed-color-707070);
+  text-align: left;
+  font: normal normal bold 12px/16px Roboto;
+  letter-spacing: 0px;
+  color: #707070;
+  opacity: 1;
+}
+</style>
---
+++ postrofit/src/components/lockerModal.vue
@@ -0,0 +1,107 @@
+<template>
+  <div class="lockerModal_blackBg">
+    <div class="lockerModal_container">
+      <div class="lockerModal_title">보관함 번호</div>
+      <div class="lockerModal_warning">
+        *타인에게 노출되지 않도록 주의하세요
+      </div>
+      <div class="lockerModal_location">강남A 3번 보관함</div>
+      <div class="lockerModal_passwd">3145</div>
+      <button class="lockerModal_button">닫기</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.lockerModal_blackBg {
+  width: 100%;
+  height: 100%;
+
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 1;
+
+  background-color: rgba(0, 0, 0, 0.4);
+}
+.lockerModal_container {
+  margin: 195px auto 0px;
+  /* Layout Properties */
+  /* top: 218px; */
+  /* left: 32px; */
+  width: 356px;
+  height: 371px;
+  /* UI Properties */
+  background: var(--unnamed-color-ffffff) 0% 0% no-repeat padding-box;
+  background: #ffffff 0% 0% no-repeat padding-box;
+  border-radius: 10px;
+  opacity: 1;
+  /* UI Properties */
+  font: var(--unnamed-font-style-normal) normal var(--unnamed-font-weight-bold)
+    30px/39px Roboto;
+  letter-spacing: var(--unnamed-character-spacing-0);
+  color: var(--unnamed-color-707070);
+  text-align: center;
+  font: normal normal bold 30px/39px Roboto;
+  letter-spacing: 0px;
+  color: #707070;
+  opacity: 1;
+}
+.lockerModal_title {
+  padding: 42px 0px 10px;
+}
+.lockerModal_warning {
+  /* UI Properties */
+  font: var(--unnamed-font-style-normal) normal normal 16px/21px Roboto;
+  letter-spacing: var(--unnamed-character-spacing-0);
+  color: var(--unnamed-color-6fbb69);
+  text-align: center;
+  font: normal normal normal 16px/21px Roboto;
+  letter-spacing: 0px;
+  color: #6fbb69;
+  opacity: 1;
+}
+.lockerModal_location {
+  margin: 54px 0px 10px;
+}
+.lockerModal_passwd {
+  margin-bottom: 44px;
+  /* UI Properties */
+  font: var(--unnamed-font-style-normal) normal var(--unnamed-font-weight-bold)
+    30px/39px Roboto;
+  letter-spacing: var(--unnamed-character-spacing-0);
+  color: var(--unnamed-color-6fbb69);
+  text-align: center;
+  font: normal normal bold 30px/39px Roboto;
+  letter-spacing: 0px;
+  color: #6fbb69;
+  opacity: 1;
+}
+.lockerModal_button {
+  border: none;
+  /* Layout Properties */
+  /* top: 520px; */
+  /* left: 72px; */
+  width: 277px;
+  height: 46px;
+  /* UI Properties */
+  background: var(--unnamed-color-6fbb69) 0% 0% no-repeat padding-box;
+  background: #6fbb69 0% 0% no-repeat padding-box;
+  border-radius: 7px;
+  opacity: 1;
+  /* UI Properties */
+  font: var(--unnamed-font-style-normal) normal var(--unnamed-font-weight-bold)
+    var(--unnamed-font-size-20) / 26px Roboto;
+  letter-spacing: var(--unnamed-character-spacing-0);
+  color: var(--unnamed-color-ffffff);
+  text-align: center;
+  font: normal normal bold 20px/26px Roboto;
+  letter-spacing: 0px;
+  color: #ffffff;
+  opacity: 1;
+}
+</style>
