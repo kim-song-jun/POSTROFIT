d2dd6e9228629c6fd6c75325e459fd1fabe88123
nulzi <kimty131@naver.com>
2022-12-21 23:40:01 +0900

    [Update] views2.0 PaySuccessPage.vue
    delete entrustMenu.vue
    add progressMenu4.vue
    add noticeBox.vue
    vw단위 변경

--- postrofit/src/views/PaySuccessPage.vue
+++ postrofit/src/views/PaySuccessPage.vue
@@ -3,22 +3,25 @@
     <MenuBar></MenuBar>
     <div class="paySuccessPage_content">
       <img src="../assets/images/dice1.png" alt="" width="76" height="76" />
-      <p class="paySuccessPage_message">결제 완료</p>
-      <p class="paySuccessPage_info">대형 4시간</p>
+      <div class="paySuccessPage_message">결제 완료</div>
+      <div class="paySuccessPage_info">대형 4시간</div>
+      <progressMenu></progressMenu>
+      <noticeBox class="paySuccessPage_noticeBox"></noticeBox>
+      <button class="paySuccessPage_button">보관함 열기</button>
     </div>
-    <EntrustMenu></EntrustMenu>
-    <button class="paySuccessPage_button">보관함 열기</button>
   </div>
 </template>
 
 <script>
 import MenuBar from "../components/MenuBar.vue";
-import EntrustMenu from "../components/entrustMenu.vue";
+import progressMenu from "../components/progressMenu4.vue";
+import noticeBox from "../components/noticeBox.vue";
 
 export default {
   components: {
     MenuBar,
-    EntrustMenu,
+    progressMenu,
+    noticeBox,
   },
 };
 </script>
@@ -30,12 +33,12 @@ export default {
   top: 20px;
 }
 .paySuccessPage_content {
-  margin: 130px 0px 0px;
+  margin-top: 20vw;
   /* UI Properties */
-  font: var(--unnamed-font-style-normal) normal var(--unnamed-font-weight-bold)
+  /* font: var(--unnamed-font-style-normal) normal var(--unnamed-font-weight-bold)
     30px/39px Roboto;
   letter-spacing: var(--unnamed-character-spacing-0);
-  color: var(--unnamed-color-707070);
+  color: var(--unnamed-color-707070); */
   text-align: center;
   font: normal normal bold 30px/39px Roboto;
   letter-spacing: 0px;
@@ -43,43 +46,36 @@ export default {
   opacity: 1;
 }
 .paySuccessPage_message {
-  margin: 20px 0px 4px;
+  margin: 7vw 0vw 1vw;
 }
 .paySuccessPage_info {
-  margin: 0;
-  padding: 0px 0px 25px;
+  padding-bottom: 4.5vw;
   /* UI Properties */
-  font: var(--unnamed-font-style-normal) normal normal 16px/21px Roboto;
+  /* font: var(--unnamed-font-style-normal) normal normal 16px/21px Roboto;
   letter-spacing: var(--unnamed-character-spacing-0);
-  color: var(--unnamed-color-6fbb69);
+  color: var(--unnamed-color-6fbb69); */
   font: normal normal normal 16px/21px Roboto;
   letter-spacing: 0px;
   color: #6fbb69;
   opacity: 1;
 }
+.paySuccessPage_noticeBox {
+  margin-left: 11.5vw;
+}
 .paySuccessPage_button {
   border: none;
-  position: fixed;
-  bottom: 20px;
-  left: 50%;
-  transform: translate(-50%, 0%);
-  /* display: block; */
-  /* margin: 38px auto 20px; */
+  margin-bottom: 5vw;
   /* Layout Properties */
-  /* top: 897px; */
-  /* left: 41px; */
-  width: 335px;
-  height: 46px;
+  width: 84vw;
+  height: 11.5vw;
   /* UI Properties */
-  background: var(--unnamed-color-6fbb69) 0% 0% no-repeat padding-box;
+  /* background: var(--unnamed-color-6fbb69) 0% 0% no-repeat padding-box; */
   background: #6fbb69 0% 0% no-repeat padding-box;
-  border-radius: 7px;
-  /* opacity: 1; */
-  /* UI Properties */
-  font: var(--unnamed-font-style-normal) normal var(--unnamed-font-weight-bold)
+  border-radius: 2vw;
+  /* font: var(--unnamed-font-style-normal) normal var(--unnamed-font-weight-bold)
     var(--unnamed-font-size-20) / 26px Roboto;
   letter-spacing: var(--unnamed-character-spacing-0);
-  color: var(--unnamed-color-ffffff);
+  color: var(--unnamed-color-ffffff); */
   text-align: center;
   font: normal normal bold 20px/26px Roboto;
   letter-spacing: 0px;
