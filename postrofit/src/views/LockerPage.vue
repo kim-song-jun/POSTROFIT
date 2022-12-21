fc1214db24408f4107f95930e26b5b87eb1cff0d
nulzi <kimty131@naver.com>
2022-12-22 00:11:13 +0900

    [Update] views2.0 LockerPage.vue
    add noticeBox
    vw로 단위 변경

--- postrofit/src/views/LockerPage.vue
+++ postrofit/src/views/LockerPage.vue
@@ -8,14 +8,7 @@
       </div>
     </div>
     <LockerInfo></LockerInfo>
-    <div class="lockerPage_price">
-      <div class="lockerPage_location">이용요금</div>
-      <div class="lockerPage_map"></div>
-    </div>
-    <div class="lockerPage_locker">
-      <div class="lockerPage_location">시설 정보</div>
-      <div class="lockerPage_map"></div>
-    </div>
+    <noticeBox class="lockerPage_noticeBox"></noticeBox>
     <button class="lockerPage_button">옮길게요</button>
   </div>
 </template>
@@ -23,11 +16,13 @@
 <script>
 import MenuBar from "../components/MenuBar.vue";
 import LockerInfo from "../components/lockerInfo.vue";
+import noticeBox from "../components/noticeBox.vue";
 
 export default {
   components: {
     MenuBar,
     LockerInfo,
+    noticeBox,
   },
 };
 </script>
@@ -39,14 +34,14 @@ export default {
   top: 20px;
 }
 .lockerPage_station {
-  margin: 28px 0px 0px 41px;
+  margin: 7vw 0vw 0vw 10vw;
 }
 .lockerPage_station_name {
   /* UI Properties */
-  font: var(--unnamed-font-style-normal) normal var(--unnamed-font-weight-bold)
+  /* font: var(--unnamed-font-style-normal) normal var(--unnamed-font-weight-bold)
     var(--unnamed-font-size-24) / var(--unnamed-line-spacing-32) Roboto;
   letter-spacing: var(--unnamed-character-spacing-0);
-  color: var(--unnamed-color-707070);
+  color: var(--unnamed-color-707070); */
   text-align: left;
   font: normal normal bold 24px/32px Roboto;
   letter-spacing: 0px;
@@ -55,69 +50,34 @@ export default {
 }
 .lockerPage_station_location {
   /* UI Properties */
-  font: var(--unnamed-font-style-normal) normal normal 10px/13px Roboto;
+  /* font: var(--unnamed-font-style-normal) normal normal 10px/13px Roboto;
   letter-spacing: var(--unnamed-character-spacing-0);
-  color: var(--unnamed-color-cfcfcf);
+  color: var(--unnamed-color-cfcfcf); */
   text-align: left;
   font: normal normal normal 10px/13px Roboto;
   letter-spacing: 0px;
   color: #cfcfcf;
   opacity: 1;
 }
-.lockerPage_locker,
-.lockerPage_price {
-  margin: 0px 0px 20px 41px;
-}
-.lockerPage_location {
-  margin: 0px 0px 14px 0px;
-  /* Layout Properties */
-  /* top: 629px; */
-  /* left: 41px; */
-  /* width: 102px; */
-  /* height: 33px; */
-  /* UI Properties */
-  font: var(--unnamed-font-style-normal) normal var(--unnamed-font-weight-bold)
-    var(--unnamed-font-size-24) / var(--unnamed-line-spacing-32) Roboto;
-  letter-spacing: var(--unnamed-character-spacing-0);
-  color: var(--unnamed-color-707070);
-  text-align: left;
-  font: normal normal bold 24px/32px Roboto;
-  letter-spacing: 0px;
-  color: #707070;
-  opacity: 1;
-}
-.lockerPage_map {
-  margin: 0px 0px 0px 2px;
-  /* Layout Properties */
-  /* top: 676px; */
-  /* left: 40px; */
-  width: 334px;
-  height: 183px;
-  /* UI Properties */
-  background: var(--unnamed-color-e4e4e4) 0% 0% no-repeat padding-box;
-  background: #e4e4e4 0% 0% no-repeat padding-box;
-  border-radius: 7px;
-  opacity: 1;
+.lockerPage_noticeBox {
+  margin-left: 10.5vw;
 }
 .lockerPage_button {
   border: none;
   display: block;
-  margin: 38px auto 20px;
+  margin: 9.5vw auto 5vw;
   /* Layout Properties */
-  /* top: 897px; */
-  /* left: 41px; */
-  width: 335px;
-  height: 46px;
+  width: 84vw;
+  height: 11.5vw;
   /* UI Properties */
-  background: var(--unnamed-color-6fbb69) 0% 0% no-repeat padding-box;
+  /* background: var(--unnamed-color-6fbb69) 0% 0% no-repeat padding-box; */
   background: #6fbb69 0% 0% no-repeat padding-box;
-  border-radius: 7px;
-  /* opacity: 1; */
-  /* UI Properties */
-  font: var(--unnamed-font-style-normal) normal var(--unnamed-font-weight-bold)
+  box-shadow: 0px 3px 6px #00000029;
+  border-radius: 2vw;
+  /* font: var(--unnamed-font-style-normal) normal var(--unnamed-font-weight-bold)
     var(--unnamed-font-size-20) / 26px Roboto;
   letter-spacing: var(--unnamed-character-spacing-0);
-  color: var(--unnamed-color-ffffff);
+  color: var(--unnamed-color-ffffff); */
   text-align: center;
   font: normal normal bold 20px/26px Roboto;
   letter-spacing: 0px;
