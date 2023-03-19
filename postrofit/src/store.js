2582993498efb9d38a3e5bb36dd698377173a4c8
kimsongjun <kinsongjun13@gmail.com>
2023-03-19 16:06:44 +0900

    F [MOD] 기존 명명규칙이 안지켜지는 것들을 잘 구별해서함, 상수는 대문자, 나머지는 camelCase

--- postrofit/src/store.js
+++ postrofit/src/store.js
@@ -3,25 +3,29 @@ import Line2 from '../src/assets/data/Line2.json';
 const store = createStore({
   state() {
     return {
-      StartStation: {},
-      EndStation: {},
-      SelectedStation: {},
-      clicked: false,
-      openSelected: false,
+      startStation: {},
+      endStation: {},
+      selectStation: {},
+      bottomMenuOpen: false,
+      bottomLockerOpen: false,
       LINE2_JSON: {},
-      LINE2_IMAGE_TRANSFORM: {
-        transfromX: 0,
-        transfromY: 0,
-      },
-      LINE2_IMAGE_SCALE: 1,
     };
   },
   mutations: {
-    setLine2ImageScale(state, scale) {
-      state.LINE2_IMAGE_SCALE = scale;
+    setBottomLockerOpen(state, bool) {
+      state.bottomLockerOpen = bool;
     },
-    setLine2ImageTransfrom(state, data) {
-      state.LINE2_IMAGE_TRANSFORM = {...data};
+    setBottomMenuOpen(state, bool) {
+      state.bottomMenuOpen = bool;
+    },
+    setSelectStation(state, selectStation) {
+      state.selectStation = {...selectStation};
+    },
+    setEndStation(state, endStation) {
+      state.endStation = {...endStation};
+    },
+    setStartStation(state, startStation) {
+      state.startStation = {...startStation};
     },
     initLine2Json(state) {
       state.LINE2_JSON = {
@@ -30,27 +34,14 @@ const store = createStore({
       };
       console.log(state.LINE2_JSON);
     },
-    setOpenSelected(state, openSelected) {
-      state.openSelected = openSelected;
-    },
-    setStartStation(state, start) {
-      state.StartStation = start;
-    },
-    setEndStation(state, end) {
-      state.EndStation = end;
-    },
-    setSelectedStation(state, selected) {
-      state.SelectedStation = selected;
-    },
-    setClicked(state, clicked) {
-      state.clicked = clicked;
-    },
   },
   actions: {
     initStation(context) {
       context.commit('setStartStation', {});
       context.commit('setEndStation', {});
-      context.commit('setSelectedStation', {});
+      context.commit('setSelectStation', {});
+      context.commit('setBottomMenuOpen', false);
+      context.commit('setBottomLockerOpen', false);
     },
   },
 });
