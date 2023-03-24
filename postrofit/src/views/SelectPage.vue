94e22d5ce2aefc976869b37380c56716aad21307
nulzi <kimty131@naver.com>
2023-03-25 01:30:58 +0900

    F [ADD] nested router를 위한 SelectPage

---
+++ postrofit/src/views/SelectPage.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="select_container">
+    <MenuBar />
+    <router-view />
+  </div>
+</template>
+
+<script>
+import MenuBar from '../components/MenuBar.vue';
+
+export default {
+  components: {
+    MenuBar,
+  },
+};
+</script>
+
+<style>
+.select_container {
+  width: 100vw;
+  height: 97.7vh;
+  padding-top: 2.3vh;
+}
+</style>
