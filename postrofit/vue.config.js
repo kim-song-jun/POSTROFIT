9bb9062ad228526f5584fbc8b19b3e57c3f913d5
nulzi-laptop <kimty131@naver.com>
2023-04-17 13:51:14 +0900

    F [MOD] CORS 문제 해결 후 다시 baseurl 설정
    첫 CORS 문제는 서버 부분이 맞았지만 그 다음 문제는 주소에 오타가 있었다.

--- postrofit/vue.config.js
+++ postrofit/vue.config.js
@@ -1,7 +1,7 @@
 const {defineConfig} = require('@vue/cli-service');
 module.exports = defineConfig({
   transpileDependencies: true,
-  devServer: {
-    proxy: 'http://54.168.210.4:8000//backend-service',
-  },
+  // devServer: {
+  //   proxy: 'http://54.168.210.4:8000/backend-service',
+  // },
 });
