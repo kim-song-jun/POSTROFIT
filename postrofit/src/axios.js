99b41251bc10f64b6e1b1d71f6ec2337a2901c28
nulzi <kimty131@naver.com>
2023-04-22 04:01:22 +0900

    F [MOD] baseUrl 수정

--- postrofit/src/axios.js
+++ postrofit/src/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const baseAxios = axios.create({
-  baseURL: 'http://54.168.210.4:8000/backend-service',
+  // baseURL: 'http://54.168.210.4:8000/backend-service',
+  baseURL: 'http://18.183.199.196:8000/backend-service/',
 });
 
 export default baseAxios;
